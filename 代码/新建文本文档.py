fc0ad309c096f446b89cc70e55feece33a9dd688
hhxhwxh0206 <50517572+hhxhwxh0206@users.noreply.github.com>
2019-06-24 11:09:31 +0800

    Add files via upload

---
+++ 代码/新建文本文档.py
@@ -0,0 +1,49 @@
+# 这是最简单的Python程序
+
+# print("你好，中国！")  # 输出数据
+# input()
+
+
+'''
+功能：这是简单的Demo程序
+作者：IT魔君
+时间：2019-6-17 14:57
+'''
+
+
+i = 3
+i = i + 2
+
+i = 3.14
+print(type(i), i)
+
+i = "中国"
+print(type(i), i)
+print(len(i))
+
+name = "你'好'啊\""
+print(name)
+
+name = '你"好"\n啊\''
+print(name)
+
+name = '''<html>
+            <head>
+            </head>
+            <body>
+            </body>
+          </html>'''
+print(name)
+
+path = r"C:\Users\dengjun\Documents\黑客马拉松比赛\duangu"
+print(path)
+
+data = b'abc\xff\x32'
+print(data)
+
+print(2 ** 3)
+print(-3 // 2)
+
+i = 3
+i += 6
+print(i)
